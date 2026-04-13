049c8bbf7d7fdb1043467438342c37eb629add2d
I IKUN2004 <164202514+I-IKUN2004@users.noreply.github.com>
2026-04-13 19:09:59 +0800

    Update PlayerSettingGUI.cc

--- src/pland/gui/PlayerSettingGUI.cc
+++ src/pland/gui/PlayerSettingGUI.cc
@@ -15,10 +15,10 @@ void PlayerSettingGUI::sendTo(Player& player) {
     auto& setting = PLand::getInstance().getLandRegistry().getOrCreatePlayerSettings(player.getUuid());
 
     auto       localeCode = player.getLocaleCode();
-    CustomForm fm(("[PLand] | 玩家设置"_trl(localeCode)));
+    CustomForm fm(("§l§d[星辰] §5个人偏好设置"_trl(localeCode)));
 
-    fm.appendToggle("showEnterLandTitle", "是否显示进入领地提示"_trl(localeCode), setting.showEnterLandTitle);
-    fm.appendToggle("showBottomContinuedTip", "是否持续显示底部提示"_trl(localeCode), setting.showBottomContinuedTip);
+    fm.appendToggle("showEnterLandTitle", "§l§2▶ 显示进入领地提示\n§r§8(跨越边界时屏幕中央弹字)"_trl(localeCode), setting.showEnterLandTitle);
+    fm.appendToggle("showBottomContinuedTip", "§l§6▶ 持续显示底部提示\n§r§8(在领地内时底部常驻显示)"_trl(localeCode), setting.showBottomContinuedTip);
 
     fm.sendTo(player, [&setting](Player& pl, CustomFormResult res, FormCancelReason) {
         if (!res) {
@@ -28,9 +28,8 @@ void PlayerSettingGUI::sendTo(Player& player) {
         setting.showEnterLandTitle     = std::get<uint64_t>(res->at("showEnterLandTitle"));
         setting.showBottomContinuedTip = std::get<uint64_t>(res->at("showBottomContinuedTip"));
 
-        feedback_utils::sendText(pl, "设置已保存"_trl(pl.getLocaleCode()));
+        feedback_utils::sendText(pl, "§e[星辰] §a个人偏好设置已成功保存！"_trl(pl.getLocaleCode()));
     });
 }
 
-
-} // namespace land::gui
+}
