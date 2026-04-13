c43d70db1232eebc0fbc1c346d0fb8a30de355ed
I IKUN2004 <164202514+I-IKUN2004@users.noreply.github.com>
2026-04-13 19:01:27 +0800

    Update LandMainMenuGUI.cc

--- src/pland/gui/LandMainMenuGUI.cc
+++ src/pland/gui/LandMainMenuGUI.cc
@@ -11,22 +11,20 @@
 
 #include <ll/api/form/SimpleForm.h>
 
-
 namespace land::gui {
-
 
 void LandMainMenuGUI::sendTo(Player& player) {
     auto localeCode = player.getLocaleCode();
 
     auto fm = ll::form::SimpleForm{};
-    fm.setTitle("[PLand] | 领地菜单"_trl(localeCode));
-    fm.setContent("欢迎使用 Pland 领地管理插件\n\n请选择一个功能"_trl(localeCode));
+    fm.setTitle("§l§d[星辰] §5领地主菜单"_trl(localeCode));
+    fm.setContent("§b✧ 欢迎使用星辰领地管理系统 ✧\n\n§7请选择您要执行的操作："_trl(localeCode));
 
-    fm.appendButton("新建领地"_trl(localeCode), "textures/ui/anvil_icon", "path", [](Player& pl) {
+    fm.appendButton("§l§a▶ 新建领地\n§r§8[ 圈占新的专属地块 ]"_trl(localeCode), "textures/ui/anvil_icon", "path", [](Player& pl) {
         NewLandGUI::sendChooseLandDim(pl);
     });
 
-    fm.appendButton("管理领地"_trl(localeCode), "textures/ui/icon_spring", "path", [](Player& pl) {
+    fm.appendButton("§l§6▶ 管理领地\n§r§8[ 设置权限与各项参数 ]"_trl(localeCode), "textures/ui/icon_spring", "path", [](Player& pl) {
         SimpleLandPicker::sendTo(
             pl,
             PLand::getInstance().getLandRegistry().getLands(pl.getUuid()),
@@ -37,18 +35,17 @@ void LandMainMenuGUI::sendTo(Player& player) {
 
     if (ConfigProvider::isLandTeleportEnabled()
         || PLand::getInstance().getLandRegistry().isOperator(player.getUuid())) {
-        fm.appendButton("领地传送"_trl(localeCode), "textures/ui/icon_recipe_nature", "path", [](Player& pl) {
+        fm.appendButton("§l§3▶ 领地传送\n§r§8[ 快速折跃至名下资产 ]"_trl(localeCode), "textures/ui/icon_recipe_nature", "path", [](Player& pl) {
             LandTeleportGUI::sendTo(pl);
         });
     }
 
-    fm.appendButton("个人设置"_trl(localeCode), "textures/ui/icon_setting", "path", [](Player& pl) {
+    fm.appendButton("§l§e▶ 个人设置\n§r§8[ 偏好与系统选项 ]"_trl(localeCode), "textures/ui/icon_setting", "path", [](Player& pl) {
         PlayerSettingGUI::sendTo(pl);
     });
 
-    fm.appendButton("关闭"_trl(localeCode), "textures/ui/cancel", "path");
+    fm.appendButton("§l§c✖ 关闭面板"_trl(localeCode), "textures/ui/cancel", "path");
     fm.sendTo(player);
 }
 
-
-} // namespace land::gui
+}
